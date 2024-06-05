6a5d279595f159c989d357447c05cd90dcf776a2
Raghav Dahal <88540324+raghav977@users.noreply.github.com>
2024-06-05 12:36:38 +0545

    Add files via upload
    Learning datatypes and its conversion in javascript

---
+++ datatype.js
@@ -0,0 +1,19 @@
+let a = "abc"
+console.log(typeof(a))
+let b = Number(a)
+console.log(b)
+
+let c = "raghav"
+let d = Boolean(c)
+console.log(d)
+
+let e = ""
+let f = Boolean(e)
+console.log(f)
+
+// summary
+// in javascript 
+// truthy values are:
+// "string",true,1,{},[],'1','-1'
+// falsy values are:
+// "",false,0,null,undefined,NaN
