1f8aad2090c96e58384c3887681b1ca03726b373
Raghav Dahal <88540324+raghav977@users.noreply.github.com>
2024-06-05 14:58:54 +0545

    objectscreation and merge
    ways to create and merge objects

---
+++ objects.js
@@ -0,0 +1,41 @@
+// // ways of declaring obejects
+// // const tinderUser = new Object()
+// const tinderUser = {}
+// tinderUser.id = "123abc"
+// tinderUser.name = "Raghav"
+// tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+// const regularUser = {
+//     email : "raghavdahal686@gmail.com",
+//     fullname:{
+//         userfullname :{
+//             firstName:"Raghav",
+//             lastName:"Dahal"
+//         }
+//     }
+// }
+// console.log(regularUser.fullname.userfullname.firstName)
+
+
+// merging objects
+const obj1 = {
+    1:"a",
+    2:"b",
+    3:"c"
+}
+const obj2 = {
+    4:"d",
+    5:"e"
+}
+
+// const obj3 = obj1+obj2
+// const obj4 = {obj1,obj2}
+// console.log(obj4)
+
+// const obj3 = Object.assign({},obj1,obj2)
+// console.log(obj3)
+
+// const obj3 = {...obj1,...obj2}
+// console.log(obj3)
